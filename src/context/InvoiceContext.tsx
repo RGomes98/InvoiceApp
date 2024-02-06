7266248ebbe3fd8f25e940261f7d89857bb3714b
Renan Gomes <renango2015@gmail.com>
2024-02-06 04:37:48 -0300

    refactor: improve invoices type definitions

--- src/context/InvoiceContext.tsx
+++ src/context/InvoiceContext.tsx
@@ -1,13 +1,13 @@
 import type { ReactNode, SetStateAction, Dispatch } from 'react';
 import { validateInvoices } from '../helpers/validateInvoices';
-import type { Invoices } from '../lib/schemas/invoice.schema';
+import type { Invoice } from '../lib/schemas/invoice.schema';
 import { createContext, useState } from 'react';
 
 export type FilterOptions = undefined | 'draft' | 'pending' | 'paid';
 
 type InvoiceContext = {
-  invoices: Invoices | null;
-  setInvoices: Dispatch<SetStateAction<Invoices | null>>;
+  invoices: Invoice[] | null;
+  setInvoices: Dispatch<SetStateAction<Invoice[] | null>>;
   isDropdownActive: boolean;
   setIsDropdownActive: Dispatch<SetStateAction<boolean>>;
   activeFilter: FilterOptions;
@@ -23,7 +23,7 @@ export const InvoiceContext = createContext<InvoiceContext | null>(null);
 const currentInvoices = validateInvoices() || null;
 
 export const InvoiceContextProvider = ({ children }: { children: ReactNode }) => {
-  const [invoices, setInvoices] = useState<Invoices | null>(currentInvoices);
+  const [invoices, setInvoices] = useState<Invoice[] | null>(currentInvoices);
   const [activeFilter, setActiveFilter] = useState<FilterOptions>(undefined);
   const [isInvoiceMenuActive, setIsInvoiceMenuActive] = useState(false);
   const [activeInvoiceId, setActiveInvoiceId] = useState<string>('');
--- src/helpers/validateInvoices.ts
+++ src/helpers/validateInvoices.ts
@@ -1,8 +1,9 @@
-import { invoicesSchema } from '../lib/schemas/invoice.schema';
+import { invoiceSchema } from '../lib/schemas/invoice.schema';
+import { z } from 'zod';
 
 export const validateInvoices = () => {
   const currentInvoices = JSON.parse(localStorage.getItem('invoices') || String(null));
-  const validInvoices = invoicesSchema.safeParse(currentInvoices);
+  const validInvoices = z.array(invoiceSchema).safeParse(currentInvoices);
 
   if (!validInvoices.success) return localStorage.setItem('invoices', String(null));
   return validInvoices.data;
