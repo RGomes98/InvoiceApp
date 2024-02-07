84880f20a14ffdd1f9f97badad74711b5efeb369
Renan Gomes <renango2015@gmail.com>
2024-02-07 18:40:02 -0300

    feat: implement mark as paid and delete functionality in Invoices

--- src/components/ActionsButtons/ActionsButtons.tsx
+++ src/components/ActionsButtons/ActionsButtons.tsx
@@ -1,16 +1,29 @@
 import { getActiveThemeStyles } from '../../utils/getActiveThemeStyles';
+import { useInvoiceActions } from '../../hooks/useInvoiceActions';
+import { useInvoiceContext } from '../../hooks/useInvoiceContext';
 import { useThemeContext } from '../../hooks/useThemeContext';
 
 import styles from './ActionsButtons.module.scss';
 
 export const ActionsButtons = () => {
+  const { deleteInvoice, markInvoiceAsPaid } = useInvoiceActions();
+  const { invoices, activeInvoiceId } = useInvoiceContext();
   const { activeTheme } = useThemeContext();
+
+  const currentInvoice = invoices?.find(({ id }) => id === activeInvoiceId);
+  if (!currentInvoice) return null;
 
   return (
     <li className={`${styles.actionItem} ${getActiveThemeStyles(styles[activeTheme])}`}>
-      <button className={styles.editButton}>Edit</button>
-      <button className={styles.deleteButton}>Delete</button>
-      <button className={styles.paidButton}>Mark as Paid</button>
+      {currentInvoice?.status !== 'paid' && <button className={styles.editButton}>Edit</button>}
+      <button className={styles.deleteButton} onClick={() => deleteInvoice(currentInvoice.id)}>
+        Delete
+      </button>
+      {currentInvoice?.status === 'pending' && (
+        <button onClick={() => markInvoiceAsPaid(currentInvoice.id)} className={styles.paidButton}>
+          Mark as Paid
+        </button>
+      )}
     </li>
   );
 };
