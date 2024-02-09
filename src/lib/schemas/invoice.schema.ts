772cbc13290acab2fec0c4cf7bccabadad62bc23
Renan Gomes <renango2015@gmail.com>
2024-02-09 16:15:24 -0300

    feat: add invoice schema error messages

--- src/lib/schemas/invoice.schema.ts
+++ src/lib/schemas/invoice.schema.ts
@@ -1,47 +1,40 @@
-import { getDaysDifference } from '../../utils/getDaysDifference';
 import { z } from 'zod';
 
 const postCodeRegExp =
   /^[A-Za-z]{1,2}\d[A-Za-z\d]? \d[A-Za-z]{2}$|^\d{5}$|^[A-Za-z]{1,2}\d{1,2} \d[A-Za-z]{2}$/;
 
-export const itemSchema = z.object({
-  name: z.string().trim().min(1),
-  quantity: z.number().positive().int(),
-  price: z.number().nonnegative(),
-  total: z.number().nonnegative(),
-});
+export const itemSchema = z
+  .array(
+    z.object({
+      name: z.string().trim().min(1, { message: 'Can’t be empty.' }),
+      quantity: z.coerce.number({ required_error: 'Can’t be empty.' }).positive().int(),
+      price: z.coerce.number({ required_error: 'Can’t be empty.' }).positive(),
+      total: z.coerce.number({ required_error: 'Can’t be empty.' }).positive(),
+    })
+  )
+  .refine((data) => data.length >= 1, { path: ['items'], message: 'An item must be added.' });
 
 export const addressSchema = z.object({
-  street: z.string().trim().min(1),
-  city: z.string().trim().min(1),
-  postCode: z.string().regex(new RegExp(postCodeRegExp)),
-  country: z.string().trim().min(1),
+  street: z.string().trim().min(1, { message: 'Can’t be empty.' }),
+  city: z.string().trim().min(1, { message: 'Can’t be empty.' }),
+  postCode: z.string().regex(new RegExp(postCodeRegExp), { message: 'Invalid code.' }),
+  country: z.string().trim().min(1, { message: 'Can’t be empty.' }),
 });
 
-export const invoiceSchema = z
-  .object({
-    id: z.string().trim().length(6),
-    createdAt: z.coerce.date(),
-    paymentDue: z.coerce.date(),
-    description: z.string().trim().min(10),
-    paymentTerms: z.number().positive().int(),
-    clientName: z.string().trim().min(1),
-    clientEmail: z.string().email(),
-    status: z.literal('draft').or(z.literal('pending')).or(z.literal('paid')),
-    senderAddress: addressSchema,
-    clientAddress: addressSchema,
-    items: z.array(itemSchema).nonempty(),
-    total: z.number().nonnegative(),
-  })
-  .superRefine(({ createdAt, paymentDue }, refinementContext) => {
-    if (getDaysDifference(createdAt, paymentDue) <= 0) {
-      refinementContext.addIssue({
-        message: "Payment due date must be at least 1 day later than today's date.",
-        code: z.ZodIssueCode.custom,
-        path: ['paymentDue'],
-      });
-    }
-  });
+export const invoiceSchema = z.object({
+  id: z.string().trim().length(6),
+  createdAt: z.string(),
+  paymentDue: z.coerce.date(),
+  description: z.string().trim().min(10, { message: 'Can’t be empty.' }),
+  paymentTerms: z.coerce.number(),
+  clientName: z.string().trim().min(1, { message: 'Can’t be empty.' }),
+  clientEmail: z.string().email({ message: 'Invalid email.' }),
+  status: z.literal('draft').or(z.literal('pending')).or(z.literal('paid')),
+  senderAddress: addressSchema,
+  clientAddress: addressSchema,
+  items: itemSchema,
+  total: z.number().positive(),
+});
 
 export type Invoice = z.infer<typeof invoiceSchema>;
 export type Address = z.infer<typeof addressSchema>;
