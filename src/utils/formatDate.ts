738e195ccb71a24cf84aa21d51a836f1080c5711
Renan Gomes <renango2015@gmail.com>
2024-02-08 22:51:37 -0300

    feat: add formatToDateTime function to enhance date formatting options

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,3 +3,16 @@ export const formatDate = (date: Date) => {
     new Date(date)
   );
 };
+
+export const formatToDateTime = (date: Date) => {
+  const [day, month, year] = Intl.DateTimeFormat('en-GB', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    timeZone: 'Europe/London',
+  })
+    .format(new Date(date))
+    .split('/');
+
+  return [year, month, day].join('-');
+};
